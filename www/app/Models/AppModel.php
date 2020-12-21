9508cdcc4d27fccb5e3435486f57735cbbde3df2
Iago Neres <iagoneresb@gmail.com>
2020-12-21 12:59:34 -0500

    main | Add Uuid Trait and base Model

---
+++ www/app/Models/AppModel.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Traits\Uuid;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+use App\Scopes\OrderByNewerScope;
+
+/**
+ * Class AppModel
+ * @package App\Entities
+ */
+class AppModel extends Model
+{
+    use SoftDeletes, Uuid;
+
+    // protected static function boot()
+    // {
+    //     parent::boot();
+    //     static::addGlobalScope(new OrderByNewerScope());
+    // }
+}
