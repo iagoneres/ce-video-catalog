d5b295a95581b33d0b8d89b50e9855458bfb6865
Iago Neres <iagoneresb@gmail.com>
2020-12-31 03:08:15 -0500

    main | Refactor Category feature test

--- www/tests/Feature/Models/CategoryTest.php
+++ www/tests/Feature/Models/CategoryTest.php
@@ -6,15 +6,26 @@ use App\Models\Category;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Str;
+use phpDocumentor\Reflection\Types\This;
 use Tests\TestCase;
 
 class CategoryTest extends TestCase
 {
     use DatabaseMigrations;
 
-    public function testCreate()
+    public function testCreateValidUuid()
     {
-        $category_data = ['name' => 'test_1'];
+        $category_data = ['name' => 'test'];
+        $category = Category::create($category_data);
+        $category->refresh();
+
+        $this->assertTrue(Str::isUuid($category->id));
+    }
+
+    public function testCreateDefaultFields()
+    {
+        $category_data = ['name' => 'test'];
         $category = Category::create($category_data);
         $category->refresh();
 
@@ -22,26 +33,32 @@ class CategoryTest extends TestCase
         $this->assertNull($category->description);
         $this->assertTrue($category->is_active);
         $this->assertNull($category->deleted_at);
+    }
 
-        $category_data = ['name' => 'test_2', 'description' => null];
+    public function testCreatePersonalizedDescription()
+    {
+        $category_data = ['name' => 'test_1', 'description' => null];
         $category = Category::create($category_data);
         $category->refresh();
         
         $this->assertNull($category->description);
 
-        $category_data = ['name' => 'test_3', 'description' => 'Test description 1'];
+        $category_data = ['name' => 'test_2', 'description' => 'Test description'];
         $category = Category::create($category_data);
         $category->refresh();
         
         $this->assertEquals($category_data['description'], $category->description);
-
-        $category_data = ['name' => 'test_4', 'is_active' => false];
+    }
+    
+    public function testCreatePersonalizedIsActive()
+    {
+        $category_data = ['name' => 'test_1', 'is_active' => false];
         $category = Category::create($category_data);
         $category->refresh();
         
         $this->assertFalse($category->is_active);
 
-        $category_data = ['name' => 'test_5', 'is_active' => true];
+        $category_data = ['name' => 'test_2', 'is_active' => true];
         $category = Category::create($category_data);
         $category->refresh();
         
@@ -76,5 +93,20 @@ class CategoryTest extends TestCase
         foreach($update_data as $key => $value) {
             $this->assertEquals($value, $category->{$key});
         }
+    }
+
+    public function testDelete()
+    {
+        $category_data = ['name' => 'test', 'description' => 'Test description'];
+        $category = Category::create($category_data);
+        $category->refresh();
+        $this->assertTrue($category->delete());
+
+        $not_deleted_category = Category::where('id', $category->id)->first();
+        $this->assertNull($not_deleted_category);
+
+        $deleted_category = Category::withTrashed()->where('id', $category->id)->first();
+        $this->assertNotEquals(null, $deleted_category->deleted_at);
+        
     }
 }
