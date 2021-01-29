9b2564e6c1301bc72650a4a394ca7f2500e38322
Iago Neres <iagoneresb@gmail.com>
2021-01-29 00:20:20 -0500

    feat: :sparkles: add video methods store and update with categories and genres

--- www/app/Http/Controllers/Api/VideoController.php
+++ www/app/Http/Controllers/Api/VideoController.php
@@ -3,10 +3,34 @@
 namespace App\Http\Controllers\Api;
 
 use App\Models\Video;
-
+use Illuminate\Http\Request;
 
 class VideoController extends BasicCrudController
 {
+    public function store(Request $request)
+    {
+        $validatedData = $this->validate($request, $this->storeRules());
+        $obj = $this->model()::create($validatedData);
+
+        $obj->categories()->sync($request->get('categories_id'));
+        $obj->genres()->sync($request->get('genres_id'));
+        
+        return $obj->refresh();
+    }
+
+    public function update(Request $request, $id)
+    {
+        $obj = $this->findOrFail($id);
+        $validatedData = $this->validate($request, $this->updateRules());
+        $obj->update($validatedData);
+
+        $obj->categories()->sync($request->get('categories_id'));
+        $obj->genres()->sync($request->get('genres_id'));
+        
+        return $obj;
+    }
+    
+    
     protected function model()
     {
         return Video::class;
@@ -21,7 +45,9 @@ class VideoController extends BasicCrudController
             "release_year" => "required|date_format:Y",
             "new_release" => "boolean",
             "rating"  => "required|in:{$array}",
-            "duration" => "required|integer"
+            "duration" => "required|integer",
+            "categories_id" => "required|array|exists:categories,id",
+            "genres_id"  => "required|array|exists:genres,id"
         ];
     }
 
@@ -34,7 +60,9 @@ class VideoController extends BasicCrudController
             "release_year" => "required|date_format:Y",
             "new_release" => "boolean",
             "rating"  => "required|in:{$array}",
-            "duration" => "required|integer"
+            "duration" => "required|integer",
+            "categories_id" => "required|array|exists:categories,id",
+            "genres_id"  => "required|array|exists:genres,id"
         ];
     }
 }
